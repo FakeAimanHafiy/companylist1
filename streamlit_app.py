7bf72cb60223010e53b887b0e39edf2f9c02762a
kenwoo29 <78064242+FakeAimanHafiy@users.noreply.github.com>
2023-09-21 12:38:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,14 +86,23 @@ if __name__ == '__main__':
             marker = folium.Marker(location=[latitude, longitude], popup=popup_name, tooltip=company_name)
             marker.add_to(map_my)
 
-            # Create a function to update the sidebar with company information when marker is clicked
-            def update_sidebar(marker=marker, sidebar_container=sidebar_container,
-                               company_name=company_name, company_address=company_address):
-                update_sidebar_container(sidebar_container, company_name, company_address)
+            # # Create a function to update the sidebar with company information when marker is clicked
+            # def update_sidebar(marker=marker, sidebar_container=sidebar_container,
+            #                    company_name=company_name, company_address=company_address):
+            #     update_sidebar_container(sidebar_container, company_name, company_address)
 
+            # # Add a click event to the marker
+            # marker.add_child(folium.ClickForMarker(popup=update_sidebar))
+            # ...
+            # Create a function to update the sidebar with company information
+            def update_sidebar(marker=marker, company_info_container=company_info_container, company_name=company_name, company_address=company_address):
+                company_info_container.write(f"**Company Name:** {company_name}")
+                company_info_container.write(f"**Company Address:** {company_address}")
+            
             # Add a click event to the marker
-            marker.add_child(folium.ClickForMarker(popup=update_sidebar))
-
+            marker.add_to(map_my)
+            marker.add_child(folium.ClickForMarker())  # Remove the 'popup' parameter            
+            # ...        
     # Specify the file name
     file_name = "MMU ITP List 13_9_9_11.xlsx"
 
