846a1d8208f6f46854bd4c467cb5db3583d2afb7
kenwoo29 <78064242+FakeAimanHafiy@users.noreply.github.com>
2023-09-21 14:30:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,20 +17,63 @@ from streamlit_folium import st_folium
 from sidebar import sidebar
 import plotly.express as px
 
-
-# 3.16000, 101.71000 : Kuala Lumpur
+# 3.16000, 101.71000: Kuala Lumpur
 
 def read_file(filename, sheetname):
     excel_file = pd.ExcelFile(filename)
     data_d = excel_file.parse(sheet_name=sheetname)
-
     return data_d
+
+def create_map(geojson_file, itp_list_state):
+    map_size = Figure(width=800, height=600)
+    map_my = folium.Map(location=[4.2105, 108.9758], zoom_start=6)
+    map_size.add_child(map_my)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+
+    itp_list_state['geometry'] = itp_list_state.apply(lambda x: Point(x['map_longitude'], x['map_latitude']), axis=1)
+    itp_list_state = gpd.GeoDataFrame(itp_list_state, geometry='geometry')
+
+    joined_data = gpd.sjoin(geojson_data, itp_list_state, op="contains").groupby(["NAME_1", "NAME_2"]).size().reset_index(name="count")
+
+    merged_gdf = geojson_data.merge(joined_data, on=["NAME_1", "NAME_2"], how="left")
+    merged_gdf['count'].fillna(0, inplace=True)
+
+    threshold_scale = [0, 1, 2, 4, 8, 16, 32, 64, 128, 200, 300, 400]
+
+    choropleth = folium.Choropleth(
+        geo_data=merged_gdf,
+        name='choropleth',
+        data=merged_gdf,
+        columns=['NAME_2', 'count'],
+        key_on='feature.properties.NAME_2',
+        fill_color='RdYlGn',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        threshold_scale=threshold_scale,
+        line_color='black',
+        legend_name='District Counts',
+        highlight=False  # Disable the darkened coloration when hovering
+    ).add_to(map_my)
+
+    folium.GeoJsonTooltip(fields=['NAME_1', 'NAME_2', 'count'], aliases=['State', 'District', 'Count']).add_to(choropleth.geojson)
+
+    text_load_state.text('Plotting ...')
+    for itp_data in itp_list_state.to_dict(orient='records'):
+        latitude = itp_data['map_latitude']
+        longitude = itp_data['map_longitude']
+        company_name = itp_data['Company name']
+        popup_name = '<strong>' + str(itp_data['Company name']) + '</strong>\n' + str(itp_data['Company address'])
+        if not math.isnan(latitude) and not math.isnan(longitude):
+            folium.Marker(location=[latitude, longitude], popup=popup_name, tooltip=company_name).add_to(map_my)
+
+    return map_my
 
 if __name__ == '__main__':
     st.title('Available ITP companies in Malaysia')
     # Call the sidebar function to include it in your app
     sidebar()
-    
 
     # Create an empty space in the sidebar to display company information
     company_info_container = st.empty()
@@ -51,7 +94,7 @@ if __name__ == '__main__':
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-    
+
     itp_list_state['geometry'] = itp_list_state.apply(lambda x: Point(x['map_longitude'], x['map_latitude']), axis=1)
     itp_list_state = gpd.GeoDataFrame(itp_list_state, geometry='geometry')
 
@@ -60,7 +103,7 @@ if __name__ == '__main__':
     merged_gdf = geojson_data.merge(joined_data, on=["NAME_1", "NAME_2"], how="left")
     merged_gdf['count'].fillna(0, inplace=True)
 
-    threshold_scale = [0, 1, 2, 4, 8, 16, 32, 64, 128, 200, 300, 400] 
+    threshold_scale = [0, 1, 2, 4, 8, 16, 32, 64, 128, 200, 300, 400]
 
     choropleth = folium.Choropleth(
         geo_data=merged_gdf,
@@ -77,7 +120,7 @@ if __name__ == '__main__':
         highlight=False  # Disable the darkened coloration when hovering
     ).add_to(map_my)
 
-    folium.GeoJsonTooltip(fields=['NAME_1','NAME_2', 'count'], aliases=['State','District', 'Count']).add_to(choropleth.geojson)
+    folium.GeoJsonTooltip(fields=['NAME_1', 'NAME_2', 'count'], aliases=['State', 'District', 'Count']).add_to(choropleth.geojson)
 
     text_load_state.text('Plotting ...')
     for itp_data in itp_list_state.to_dict(orient='records'):
@@ -87,49 +130,27 @@ if __name__ == '__main__':
         popup_name = '<strong>' + str(itp_data['Company name']) + '</strong>\n' + str(itp_data['Company address'])
         if not math.isnan(latitude) and not math.isnan(longitude):
             folium.Marker(location=[latitude, longitude], popup=popup_name, tooltip=company_name).add_to(map_my)
-    # for itp_data in itp_list_state.to_dict(orient='records'):
-    #     latitude = itp_data['map_latitude']
-    #     longitude = itp_data['map_longitude']
-    #     company_name = itp_data['Company name']
-    #     company_address = itp_data['Company address']
-    #     popup_name = '<strong>' + str(company_name) + '</strong>\n' + str(company_address)
-    #     if not math.isnan(latitude) and not math.isnan(longitude):
-    #         marker = folium.Marker(location=[latitude, longitude], popup=popup_name, tooltip=company_name)
-    #         marker.add_to(map_my)
-
-    #         # Create a function to update the sidebar with company information when marker is clicked
-    #         def update_sidebar(marker=marker, company_info_container=company_info_container):
-    #             company_info_container.write(f"**Company Name:** {company_name}")
-    #             company_info_container.write(f"**Company Address:** {company_address}")
-
-    #         # Add a click event to the marker
-    #         marker.add_to(map_my)
-    #         marker.add_child(folium.ClickForMarker(popup=update_sidebar))
-
-
-
 
     # Specify the file name
     from data_processing import process_data, generate_bar_chart
-    
+
     # Specify the file name
     file_name = "MMU ITP List 13_9_9_11.xlsx"
-    
+
     # Data processing
     df = process_data(file_name)
-    
+
     # Sidebar for state selection
     if df is not None:
         selected_state = st.sidebar.selectbox("Select a State", df['STATE'].unique())
     else:
         selected_state = None
-    
+
     # Generate the bar chart
     fig = generate_bar_chart(df, selected_state)
-    
-    # Display the plotly figure
+
+    # Create and display the map
+    map_my = create_map(geojson_file, itp_list_state)
     st.title('Company Per District')
     st.plotly_chart(fig)
-
-
-#python -m streamlit run map_area.py
+    components.html(map_my._repr_html_(), height=600)
